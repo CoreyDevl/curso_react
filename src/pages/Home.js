0a98ad918e1de49f2e72a19ebf79ba9cc2fef409
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-11-04 20:30:42 -0300

    pagina Home Concluida em aula

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
+import style from '../components/layout/Home.module.css'
+import savings from '../../src/img/savings.svg'
+import LinkButton from '../components/layout/LinkButton'
+
 function Home(){
 return(
-<div>
-    <h1>Home</h1>
-    <p>conteúdo da página</p>
-</div>
+<section className={style.homeContainer}>
+    <h1>Bem-vindo ao <span>Costs</span></h1>
+    <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+    <LinkButton to="/newproject" text="Criar Projeto!"/>
+     <img src={savings} alt="costs" />
+</section>
 )
 }
 
