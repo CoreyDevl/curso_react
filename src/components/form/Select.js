aee5bb630affb1815485998c8bcc2d6eb6b23b8f
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-11-07 18:38:22 -0300

    em andamento

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -4,8 +4,15 @@ function Select({ text, name, options, handleOnChange, value}){
     return (
         <div className={styles.formControl}>
             <label htmlFor={name}>{text}</label>
-            <select name={name} id={name}></select>
+            <select name={name} 
+            id={name} 
+            onChange={handleOnChange} 
+            value={value || ''}>
             <option>Selecione uma opção</option>
+            {options.map((option)=>(
+                <option value={option.id} key={option.id}>{option.name}</option> 
+            ))}
+            </select>
         </div>
     )
 }
--- src/pages/NewProject.js
+++ src/pages/NewProject.js
@@ -1,12 +1,36 @@
 import styles from '../../src/components/layout/NewProject.module.css'
 import ProjectForm from '../components/projects/ProjectForm'
 
+import {useHistory} from 'react-router-dom'
 function NewProject(){
+
+    const history = useHistory()
+function createPost(project){
+    //initialize cost and services
+    project.cost=0
+    project.services=[]
+
+    fetch("http://localhost:5000/projects", {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify(project),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+         console.log(data)
+         //depois tem o Redirect
+    })
+    .catch(err => console.log(err))
+}
+
+
 return(
 <div className={styles.newProjectContainer}>
     <h1>Criar Projeto</h1>
     <p>Crie seu projeto para depois adicionar os serviço</p>
-    <ProjectForm btnText="Criar Projeto"/>
+    <ProjectForm handleSubmit={createPost} btnText="Criar Projeto"/>
 </div>
 )
 }
