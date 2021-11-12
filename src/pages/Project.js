bb952b9bc819c1f634f5d00639bf8fa55ff0b621
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-11-12 20:10:58 -0300

    adicionado serviços  e logicas de adição de serviços

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,15 +1,21 @@
-import styles from './Project.module.css'
+import {parse, v4 as uuidv4} from 'uuid'
 import {useParams} from 'react-router-dom'
 import {useState, useEffect} from 'react'
+import styles from './Project.module.css'
 import Loading from '../components/layout/Loading'
 import Container from '../components/layout/Container'
+import ProjectForm from '../components/projects/ProjectForm'
+import Message from '../components/layout/Message'
+import ServiceForm from '../components/service/ServiceForm'
 
 function Project(){
 
 const {id} = useParams()
 const [project, setProject] = useState([])
 const [showProjectForm, setShowProjectForm] = useState(false)
-
+const [showServiceForm, setShowServiceForm] = useState(false)
+const [message, setMessage] = useState()
+const [type, setType] = useState()
 
 useEffect(() => {
  setTimeout(() => {
@@ -25,41 +31,122 @@ useEffect(() => {
  }, 3000)
 }, [id])
 
-    function togleProjectForm(){
+    function editPost(project){
+        setMessage('')
+      //validatio budget
+     if(project.budget < project.cost) {
+         setMessage('O orçamento não pode ser menor que o custo do projeto!')
+         setType('error')
+         return false
+     }
+
+     fetch(`http://localhost:5000/projects/${project.id}`,{
+         method: 'PATCH',
+         headers: {'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(project),
+     })
+        .then(res => res.json())
+        .then((data) => {
+            setProject(data)
+            setShowProjectForm(false)
+            setMessage('Projeto atualizado!')
+            setType('success')
+        })
+        .catch(err => console.log(err))
+    }
+
+    function toggleProjectForm(){
         setShowProjectForm(!showProjectForm)
     }
- 
+    function toggleServiceForm(){
+        setShowServiceForm(!showServiceForm)
+    }
+
+    function createService(project){
+        setMessage('')
+        // lastService
+        const lastService = project.services[project.services.length - 1]
+        lastService.id = uuidv4()
+
+        const lastServiceCost = lastService.cost
+        const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+        //maximum value validation
+        if(newCost > parseFloat(project.budget)){
+            setMessage('Orçamento ultrapassado. Verifique o valor do serviço')
+            setType('error')
+            project.services.pop()
+            return false
+        }
+        //add service cost to project total cost
+        project.cost = newCost
+        // update project
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers:{
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(project)
+        })
+        .then(res => res.json())
+        .then((data => {
+            //exibir os serviços
+            console.log(data)
+        }))
+        .catch(err => console.log(err))
+    }
+
     return (
     <>
-       {project.name ? (
-       <div className={styles.project_details}>
-           <Container className="column">
-               <div className={styles.details_container}>
-                   <h1>Projeto: {project.name}</h1>
-                   <button onClick={togleProjectForm}>
-                       {!showProjectForm ? 'Editar projeto': 'Fechar'}
-                    </button>
-                    {!showProjectForm ? (
-                        <div className={styles.project_details}>
-                            <p>
-                                <span>Categoria:</span> {project.category.name}
-                            </p>
-                            <p>
-                                <span>Total de Orçamento:</span> R${project.budget}
-                            </p>
-                            <p>
-                                <span>Total de Utilizado:</span> R${project.cost}
-                            </p>
-                        </div>
-                    ):(
-                        <div>
-                        <p>detalhes do projeto</p>
-                        </div>
-                    )}
-               </div>
-           </Container>
-       </div> 
-        ):(  <Loading />
+{project.name ? (
+<div className={styles.project_details}>
+<Container className="column">
+    {message && <Message type={type} msg={message} />}
+    <div className={styles.details_container}>
+        <h1>Projeto: {project.name}</h1>
+        <button className={styles.btn} onClick={toggleProjectForm}>
+            {!showProjectForm ? 'Editar projeto': 'Fechar'}
+        </button>
+        {!showProjectForm ? (
+            <div className={styles.project_info}>
+                <p>
+                    <span>Categoria:</span> {project.category.name}
+                </p>
+                <p>
+                    <span>Total de Orçamento:</span> R${project.budget}
+                </p>
+                <p>
+                    <span>Total de Utilizado:</span> R${project.cost}
+                </p>
+            </div>
+        ):(
+            <div className={styles.project_info}>
+            <ProjectForm handleSubmit={editPost} btnText="Concluir edição" projectData={project} />
+            </div>
+        )}
+    </div>
+    <div className={styles.service_form_container}>
+        <h2>Adicione um serviço:</h2>
+        <button className={styles.btn} onClick={toggleServiceForm}>
+            {!showServiceForm ? 'Adicionar serviço': 'Fechar'}
+        </button>
+    </div>
+    <div className={styles.project_info}>
+    {showServiceForm && (
+        <ServiceForm 
+        handleSubmit={createService}
+        btnText="Adicionar serviço"
+        projectData={project}
+        />
+    )}
+    </div>
+    <h2>Serviços:</h2>
+    <Container customClass="start">
+    <p>Itens de Serviços</p>
+    </Container>
+</Container>
+</div> 
+):(  <Loading />
        )} 
    </>
     )
